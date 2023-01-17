6f0417c27429ac77d5626f5f6a11596f76e5f4fe
kiranacharya26 <acharya.kiran26ka@gmail.com>
2023-01-17 17:18:49 +0530

    fix path

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Details from "./Details";
 import { useState } from "react";
-import AdoptedPetContext from "./adoptedPetContext";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
